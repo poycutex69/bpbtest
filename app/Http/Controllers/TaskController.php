05506a4cdd0520bcb7b710f62372551cf2a49f4c
poycutex69 <poycutex69@gmail.com>
2021-01-15 01:00:25 +0800

    initial commit with task list done

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {        
+        $user = auth()->user();  
+        $data = Task::where('user_id',$user->id)->get();
+        return Inertia::render('Tasks', ['data' => $data]);
+    }
+  
+    /**
+     * Create a task
+     *
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'description' => ['required'],
+        ])->validate();
+  
+        
+
+        $user = auth()->user();        
+
+        $task = new Task();
+        $task->title        = $request->title;
+        $task->description  = $request->description;
+        $task->user_id      = $user->id;  
+        $task->save();       
+  
+        return redirect()->back()
+                    ->with('message', 'Task was created successfully.');
+    }
+  
+    /**
+     * update a task
+     *
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+        Validator::make($request->all(), [
+            'title' => ['required'],
+            'description' => ['required'],
+        ])->validate();
+  
+        if ($request->has('id')) {
+            $task = Task::find($request->input('id'));
+            
+            $task->title        = $request->title;
+            $task->description  = $request->description;
+            $task->save();
+
+            return redirect()->back()
+                    ->with('message', 'Task was updated successfully.');
+        }
+    }
+  
+    /**
+     * Delete a task
+     *
+     * @return Response
+     */
+    public function destroy(Request $request)
+    {
+        if ($request->has('id')) {
+            Task::find($request->input('id'))->delete();
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Update task status
+     * 
+     * @return Response
+     */
+    public function updatestatus(Request $request){
+        if ($request->has('id')) {
+            $task = Task::find($request->input('id'));
+            
+            $task->status       = $request->status;            
+            $task->save();
+
+            return redirect()->back()
+                    ->with('message', 'Task status was updated successfully.');
+        }
+    }
+}
---
+++ database/migrations/2021_01_14_134536_create_task_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->tinyInteger('status')->default(0);
+            $table->foreignId('user_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
